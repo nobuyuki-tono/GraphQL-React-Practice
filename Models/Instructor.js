1667aaf2770cc543d873a320c6bd81a8f7c6439e
Nobu <rockmusic2090@gmail.com>
2020-03-19 12:53:58 -0700

    Created Instructor Schema for MongoDB

---
+++ Models/Instructor.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const InstructorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = Instructor = mongoose.model("Instructo", InstructorSchema);
